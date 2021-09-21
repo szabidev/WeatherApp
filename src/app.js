60e0225efad4b0e8e0e129ebafbabab2695a6774
Szabi Vojtek <szabi.vojtek@gmail.com>
2021-09-21 18:16:27 +0300

    Finished requests

--- src/app.js
+++ src/app.js
@@ -4,29 +4,41 @@ const currentDay = new Date().toLocaleString('en-us', { weekday: 'long' });
 // Get current time
 const today = new Date();
 const dayDate = String(today.getDate()).padStart(2, '0');
-const month = String(today.getMonth() + 1).padStart(2, '0') // + 1 because January is 0
+const month = String(today.getMonth() + 1).padStart(2, '0'); // + 1 because January is 0
 const year = today.getFullYear();
-const currentDate = `${dayDate}/${month}/${year}`
+const currentDate = `${dayDate}/${month}/${year}`;
 
 // DOM elements
 const container = document.querySelector('.container');
 const form = document.querySelector('.form-control');
 const searchInput = document.querySelector('.form-input');
-// const weatherBtn = document.querySelector('.weather-btn');
 const forecastBtn = document.querySelector('.forecast-btn');
-// const card = document.getElementById('card');
+
+const canvas = document.querySelector('.canvas');
 const setIcon = document.querySelector('.icon');
 const city = document.querySelector('.city');
 const sunriseText = document.querySelector('.sunrise');
 const sunsetText = document.querySelector('.sunset');
 const countryText = document.querySelector('.country');
 // make it clickable to change from celsius to fahrenheit
-const degree = document.querySelector('.degree');
-const unit = document.querySelector('.unit');
+const degreeText = document.querySelector('.degree');
 const descriptionText = document.querySelector('.description');
 const day = document.querySelector('.day');
 const date = document.querySelector('.date');
-const googleMap = document.querySelector('.google-map-container');
+
+const forecastContainer = document.querySelector('.forecast-container');
+const forecast = document.querySelector('.forecast');
+const forecastDate = document.querySelector('.forecast-date');
+const forecastTemp = document.querySelector('.forecast-temperature');
+const forecastTime = document.querySelector('.time-string');
+
+const googleMap = document.getElementById('map');
+
+//  Create Small Icons for sunrise and sunset
+let skycons = new Skycons({ color: "white" });
+skycons.set("clear_day", Skycons.CLEAR_DAY);
+skycons.set("clear_night", Skycons.CLEAR_NIGHT);
+skycons.play();
 
 
 // add an event listener to display the current weather for the users location
@@ -50,7 +62,7 @@ window.addEventListener('load', () => {
                     return response.json();
                 })
                 .then((data) => {
-                    // Extract the valuse from the data object
+                    // Extract the values from the data object
                     const name = data.name;
                     const celsius = Math.round(data.main.temp);
                     // convert celsius to fahrenheit
@@ -60,13 +72,11 @@ window.addEventListener('load', () => {
                     // Convert the two dates to GMT 
                     const sunrise = new Date(data.sys.sunrise * 1000);
                     const sunset = new Date(data.sys.sunset * 1000);
-                    // console.log(name, temperature, country, description);
-                    // console.log(sunrise, sunset);
                     const icon = data.weather[0].icon;
                     // Update the DOM with the informations I got back
                     city.textContent = name;
                     countryText.textContent = country;
-                    degree.textContent = celsius + ` \xB0C`;
+                    degreeText.textContent = celsius + ` \xB0C`;
                     descriptionText.textContent = description;
                     day.textContent = currentDay;
                     date.textContent = currentDate;
@@ -75,18 +85,17 @@ window.addEventListener('load', () => {
                     sunsetText.textContent = `${sunset.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
                     // Set the icon
                     setIcons(icon, setIcon);
-
+                    // Set the background
+                    changeBackground(icon);
                     // Change between celsius and fahrenheit
-                    degree.addEventListener('click', () => {
-                        if (degree.textContent == celsius + ` \xB0C`) {
-                            degree.textContent = fahrenheit + ` \xB0F`;
+                    degreeText.addEventListener('click', () => {
+                        if (degreeText.textContent == celsius + ` \xB0C`) {
+                            degreeText.textContent = fahrenheit + ` \xB0F`;
                         } else {
-                            degree.textContent = celsius + ` \xB0C`;
+                            degreeText.textContent = celsius + ` \xB0C`;
                         }
                     });
-
-                    // initMap()
-                })
+                });
         });
     };
 
@@ -103,77 +112,107 @@ form.addEventListener('submit', (e) => {
             return response.json();
         })
         .then((data) => {
-            // Extract the valuse from the data object
             const name = data.name;
             const celsius = Math.round(data.main.temp);
-            // convert celsius to fahrenheit
-            const fahrenheit = (celsius * 9) / 5 + 32;
             const country = data.sys.country;
             const description = data.weather[0].description;
-            // Convert the two dates to GMT 
             const sunrise = new Date(data.sys.sunrise * 1000);
             const sunset = new Date(data.sys.sunset * 1000);
-            // console.log(name, temperature, country, description);
-            // console.log(sunrise, sunset);
             const icon = data.weather[0].icon;
-            // Update the DOM with the informations I got back
             city.textContent = name;
             countryText.textContent = country;
-            degree.textContent = celsius + ` \xB0C`;
+            degreeText.textContent = celsius + ` \xB0C`;
             descriptionText.textContent = description;
             day.textContent = currentDay;
             date.textContent = currentDate;
-            // convert the date to string, display only the hour and the minutes
             sunriseText.textContent = `${sunrise.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
             sunsetText.textContent = `${sunset.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
-            // Set the icon
             setIcons(icon, setIcon);
+            changeBackground(icon);
             searchInput.value = '';
-
-
-        })
+            forecastContainer.innerHTML = '';
+        });
 });
 
 forecastBtn.addEventListener('click', () => {
-    const searchForACity = searchInput.value;
+    const searchForACity = city.textContent;
     const api = `https://api.openweathermap.org/data/2.5/forecast?appid=${apiKey}&units=metric&q=${searchForACity}`;
+    forecastContainer.innerHTML = '';
+    console.log(api);
     fetch(api)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
+            for (let i = 0; i < data.list.length; i++) {
+                const icon = data.list[i].weather[0].icon;
+                const temperature = Math.round(data.list[i].main.temp);
+                // const fahrenheit = (temperature * 9) / 5 + 32;
+                const description = data.list[i].weather[0].description;
+                const today = new Date(data.list[0].dt * 1000).getDate();
+                const timeString = data.list[i].dt_txt;
+                const time = timeString.substring(11, 16);
+                // use this date, date variable above gives incorrect date
+                const date = timeString.substring(8, 10);
+                const forecastIcon = document.querySelector('.forecast-icon');
+                let output = document.createElement('div');
+                output.classList.add('forecast');
+                output.innerHTML = `
+                <canvas class="forecast-icon" width="32" height="32" style="color: 'var(--dark-color)'"></canvas>
+                <p class="forecast-date">${date}.</p>
+                <p class="forecast-description">${description}</p>
+                <p class="forecast-temperature">${temperature} \xB0C</p>
+                <p class="time-string">${time}</p>
+                `;
 
-        })
+                if (date == today) {
+                    output.style.gridRow = '1';
+                } else if (date == today + 1) {
+                    output.style.gridRow = '2';
+                } else if (date == today + 2) {
+                    output.style.gridRow = '3';
+                } else if (date == today + 3) {
+                    output.style.gridRow = '4';
+                } else if (date == today + 4) {
+                    output.style.gridRow = '5';
+                } else if (date == today + 5) {
+                    output.style.gridRow = '6';
+                }
+                forecastContainer.appendChild(output);
+                setIcons(icon, forecastIcon);
+
+                // degreeText.addEventListener('click', () => {
+                //     if (forecastContainer) {
+                //         if (forecastTemp.textContent == temperature + ` \xB0C`) {
+                //             forecastTemp.textContent = fahrenheit + ` \xB0F`;
+                //         } else {
+                //             forecastTemp.textContent = temperature + ` \xB0C`;
+                //         }
+                //     }
+                // });
+            };
+        });
 });
 
-// function to set the icon and background image
 const setIcons = (icon, iconId) => {
     const skycons = new Skycons({ color: 'white' });
     let currentIcon;
     if (icon === '01d') {
         currentIcon = "CLEAR_DAY";
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '01n') {
         currentIcon = 'CLEAR_NIGHT';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '02d') {
         currentIcon = 'PARTLY_CLOUDY_DAY';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '02n') {
         currentIcon = 'PARTLY_CLOUDY_NIGHT';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '03d' || icon === '03n' || icon === '04d' || icon === '04n') {
         currentIcon = 'CLOUDY';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '09d' || icon === '09n' || icon === '10d' || icon === '10n' || icon === '11d' || icon === '11n') {
         currentIcon = 'RAIN';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '13d' || icon === '13n') {
         currentIcon = 'SNOW';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     } else if (icon === '50d' || icon === '50n') {
         currentIcon = 'FOG';
-        container.style.backgroundImage = `url('./css/images/${currentIcon}.jpg')`;
     }
     // start the animation
     skycons.play()
@@ -181,17 +220,76 @@ const setIcons = (icon, iconId) => {
     return skycons.set(iconId, Skycons[currentIcon]);
 }
 
-const initMap = () => {
-    let location = { latitude: 46.771210, longitude: 23.623634 };
-    let map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 4,
-        center: location
-    })
+const changeBackground = (icon) => {
+    if (icon === '01d') {
+        container.style.backgroundImage = `url('./css/images/CLEAR_DAY.jpg')`;
+        changeFontToBlack();
+    } else if (icon === '01n') {
+        container.style.backgroundImage = `url('./css/images/CLEAR_NIGHT.jpg')`;
+        changeFontToGray();
+    } else if (icon === '02d') {
+        container.style.backgroundImage = `url('./css/images/PARTLY_CLOUDY_DAY.jpg')`;
+        changeFontToBlack()
+    } else if (icon === '02n') {
+        container.style.backgroundImage = `url('./css/images/PARTLY_CLOUDY_NIGHT.jpg')`;
+        changeFontToGray()
+    } else if (icon === '03d' || icon === '03n' || icon === '04d' || icon === '04n') {
+        container.style.backgroundImage = `url('./css/images/CLOUDY.jpg')`;
+        changeFontToGray();
+    } else if (icon === '09d' || icon === '09n' || icon === '10d' || icon === '10n' || icon === '11d' || icon === '11n') {
+        container.style.backgroundImage = `url('./css/images/RAIN.jpg')`;
+        changeFontToBlack();
+    } else if (icon === '13d' || icon === '13n') {
+        container.style.backgroundImage = `url('./css/images/SNOW.jpg')`;
+        changeFontToGray();
+    } else if (icon === '50d' || icon === '50n') {
+        container.style.backgroundImage = `url('./css/images/FOG.jpg')`;
+        changeFontToBlack();
+    }
+};
+
+const changeFontToBlack = () => {
+    city.style.color = 'var(--dark-color)';
+    countryText.style.color = 'var(--dark-color)';
+    degreeText.style.color = 'var(--dark-color)';
+    descriptionText.style.color = 'var(--dark-color)';
+    day.style.color = 'var(--dark-color)';
+    date.style.color = 'var(--dark-color)';
+    sunriseText.style.color = 'var(--dark-color)';
+    sunsetText.style.color = 'var(--dark-color)';
+
+}
+
+const changeFontToGray = () => {
+    city.style.color = 'var(--gray-color-3)'
+    countryText.style.color = 'var(--gray-color-3)';
+    degreeText.style.color = 'var(--gray-color-3)';
+    descriptionText.style.color = 'var(--gray-color-3)';
+    day.style.color = 'var(--gray-color-3)';
+    date.style.color = 'var(--gray-color-3)';
+    sunriseText.style.color = 'var(--gray-color-3)';
+    sunsetText.style.color = 'var(--gray-color-3)';
+    if (forecastContainer != null) {
+        console.log(forecastContainer.querySelector('.forecast'));
+    }
 }
 
 
-//  Create Small Icons for sunrise and sunset
-let skycons = new Skycons({ color: "white" });
-skycons.set("clear_day", Skycons.CLEAR_DAY);
-skycons.set("clear_night", Skycons.CLEAR_NIGHT);
-skycons.play();
+// const changeIconColor = (iconId) => {
+//     const left = iconId.getBoundingClientRect().left;
+//     const top = iconId.getBoundingClientRect().top;
+//     const width = iconId.getBoundingClientRect().width;
+//     const height = iconId.getBoundingClientRect().height;
+//     const ctx = iconId.getContext('2d');
+//     ctx.fillStyle = '#343A40';
+//     ctx.fillRect(left, top, width, height);
+//     console.log(left, top, width, height)
+//     console.log(ctx);
+// }
+
+
+// Typing problem, ex. cluj napoca gives error - cluj-napoca works -resolve
+// Problem adding google map to webpage
+// DRY
+// Problem changing Icon color
+// On click on degree change the values to fahrenheit in forecast containers
